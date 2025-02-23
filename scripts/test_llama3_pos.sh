f6e55355a60771472f0b68dc45ba00ba6e9e6070
Chen Zhang <59479068+e0397123@users.noreply.github.com>
2025-02-23 16:14:37 +0800

    Update test_llama3_pos.sh

--- scripts/test_llama3_pos.sh
+++ scripts/test_llama3_pos.sh
@@ -7,7 +7,7 @@ for fname in "shp" "helpful_base" "helpful_online" "helpful_rejection" "alpaca_f
     /home/dialogue/miniconda3/envs/llama_factory/bin/python run_llama3.py \
         --model "meta-llama/Llama-3-8B-Instruct" \
         --input_fname "data/${fname}_accept.json" \
-        --dimension ${dimension}
+        --dimension ${dimension} \
         --output_fname "llama3_outputs/${fname}_accept_${dimension}.jsonl";
 
 done
