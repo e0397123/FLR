fe53476a34e9d101f41278a510656f2256ed1433
Chen Zhang <59479068+e0397123@users.noreply.github.com>
2025-02-23 16:12:00 +0800

    Update and rename test_qwen2_neg.sh to test_llama3_neg.sh

---
+++ scripts/test_llama3_neg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p llama3_outputs/
+
+for fname in "shp" "helpful_base" "helpful_online" "helpful_rejection" "alpaca_farm" "beavertails_holistic"; do
+
+    /home/dialogue/miniconda3/envs/llama_factory/bin/python run_llama3.py \
+        --model "meta-llama/Llama-3-8B-Instruct" \
+        --input_fname "data/${fname}_reject.json" \
+        --output_fname "llama3_outputs/${fname}_reject.jsonl";
+
+done
