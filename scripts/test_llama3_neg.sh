5d3cc4b88f7e8263fbfdadc9373e4a780a34c425
Chen Zhang <59479068+e0397123@users.noreply.github.com>
2025-02-23 16:14:23 +0800

    Update test_llama3_neg.sh

--- scripts/test_llama3_neg.sh
+++ scripts/test_llama3_neg.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 mkdir -p llama3_outputs/
-
+export dimension="understanding"
 for fname in "shp" "helpful_base" "helpful_online" "helpful_rejection" "alpaca_farm" "beavertails_holistic"; do
 
     /home/dialogue/miniconda3/envs/llama_factory/bin/python run_llama3.py \
         --model "meta-llama/Llama-3-8B-Instruct" \
         --input_fname "data/${fname}_reject.json" \
-        --output_fname "llama3_outputs/${fname}_reject.jsonl";
+        --dimension ${dimension} \
+        --output_fname "llama3_outputs/${fname}_reject_${dimension}.jsonl";
 
 done
